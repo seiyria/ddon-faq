18678cb0a56fa02351b34d3f3738f50e02ffddf8
Kyle Kemp <kyle@seiyria.com>
2017-11-19 20:37:51 -0600

    add some basic faq and search

--- index.js
+++ index.js
@@ -3,15 +3,29 @@ var vue = new Vue({
     el: '#app',
     data: {
         faqs: [],
+        search: '',
         loading: true
     },
     methods: {
+        formatName(str) {
+            return _.kebabCase(str);
+        },
+        matchesSearch(faq) {
+            var search = this.search ? this.search.trim().toLowerCase() : '';
+
+            if(!search) return true;
+            if(_.some(faq.tags, tag => _.includes(tag.toLowerCase(), search))) return true;
+            if(_.includes(faq.question.toLowerCase(), search)) return true;
+            if(_.includes(faq.answer.toLowerCase(), search)) return true;
+
+            return false;
+        }
     }
 });
 
 axios.get('faq.yml')
     .then(res => {
         var allData = YAML.parse(res.data);
-        vue.faq = allData.faq;
+        vue.faqs = allData.faqs;
         vue.loading = false;
     });
